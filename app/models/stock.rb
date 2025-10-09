f895b03ddae5afbc84b26bf8892b9a644c5d3ff5
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-09 17:43:27 +0500

    Create and display stock object in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
         quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
-        quote.price
+        #quote.price
+        company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
+        overview = company.overview     
+
+        new(ticker: ticker_symbol, name: overview.name, last_price: quote.price(ticker_symbol))
   end
 end
