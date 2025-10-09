bf4bf1f0ebd0998286c2d60b2a54451214dbe40c
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-09 18:09:20 +0500

    Deal with invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,15 @@
 class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
+      begin
         quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
         #quote.price
         company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
         overview = company.overview     
-
+      
         new(ticker: ticker_symbol, name: overview.name, last_price: quote.price(ticker_symbol))
+      rescue => exception
+        return nil
+      end
   end
 end
