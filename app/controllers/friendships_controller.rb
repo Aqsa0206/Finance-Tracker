ed5226160f713a0341f05c17d7d4704b50796b09
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-10 22:05:59 +0500

    Display search results in browser

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -14,8 +14,9 @@ class FriendshipsController < ApplicationController
 
     def search
         if params[:friend].present?
-            @friend = params[:friend]
-            if @friend 
+            @friends = User.search(params[:friend])
+            @friends = current_user.except_current_user(@friends)
+            if @friends
                 respond_to do |format|
                 format.js { render partial: 'friendships/friend_result' }
                 end
